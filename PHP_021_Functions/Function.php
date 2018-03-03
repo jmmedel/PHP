d4ee5fa53eb69462e76c351255f70210b9f26ffd
john <jm.kagaya@gmail.com>
2018-03-04 07:19:36 +0900

    Done

--- PHP_021_Functions/Function.php
+++ PHP_021_Functions/Function.php
@@ -1,1 +1,28 @@
-<?php
+<!-- 
+PHP 5 Functions
+The real power of PHP comes from its functions; it has more than 1000 built-in functions.
+
+PHP User Defined Functions
+Besides the built-in PHP functions, we can create our own functions.
+
+A function is a block of statements that can be used repeatedly in a program.
+
+A function will not execute immediately when a page loads.
+
+A function will be executed by a call to the function.
+
+Create a User Defined Function in PHP
+A user defined function declaration starts with the word "function":
+
+Syntax
+function functionName() {
+    code to be executed;
+}
+Note: A function name can start with a letter or underscore (not a number).
+
+Tip: Give the function a name that reflects what the function does!
+
+Function names are NOT case-sensitive.
+
+In the example below, we create a function named "writeMsg()". The opening curly brace ( { ) indicates the beginning of the function code and the closing curly brace ( } ) indicates the end of the function. The function outputs "Hello world!". To call the function, just write its name:
+ -->
