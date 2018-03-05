437a6ec6f979ac08ee433ca0f0c859da933c61be
john <jm.kagaya@gmail.com>
2018-03-05 23:34:28 +0900

    Not Done

---
+++ PHP_055_Cookies/newfile.php
@@ -0,0 +1,16 @@
+
+<!-- 
+PHP Cookies
+A cookie is often used to identify a user.
+
+What is a Cookie?
+A cookie is often used to identify a user. A cookie is a small file that the server embeds on the user's computer. Each time the same computer requests a page with a browser, it will send the cookie too. With PHP, you can both create and retrieve cookie values.
+
+Create Cookies With PHP
+A cookie is created with the setcookie() function.
+
+Syntax
+setcookie(name, value, expire, path, domain, secure, httponly);
+Only the name parameter is required. All other parameters are optional.
+
+ -->
