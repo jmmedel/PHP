29fe6745b2b4e4e7cbc7946ad8cb1cac288e4a80
john <jm.kagaya@gmail.com>
2018-03-06 19:39:27 +0900

    Done

--- PHP_055_Cookies/newfile.php
+++ PHP_055_Cookies/newfile.php
@@ -13,4 +13,40 @@ Syntax
 setcookie(name, value, expire, path, domain, secure, httponly);
 Only the name parameter is required. All other parameters are optional.
 
+PHP Create/Retrieve a Cookie
+The following example creates a cookie named "user" with the value "John Doe". The cookie will expire after 30 days (86400 * 30). The "/" means that the cookie is available in entire website (otherwise, select the directory you prefer).
+
+We then retrieve the value of the cookie "user" (using the global variable $_COOKIE). We also use the isset() function to find out if the cookie is set:
+
+ -->
+
+ <!DOCTYPE html>
+<?php
+$cookie_name = "user";
+$cookie_value = "Kagaya";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+?>
+<html>
+<body>
+
+<?php
+if(!isset($_COOKIE[$cookie_name])) {
+     echo "Cookie named '" . $cookie_name . "' is not set!";
+} else {
+     echo "Cookie '" . $cookie_name . "' is set!<br>";
+     echo "Value is: " . $_COOKIE[$cookie_name];
+}
+?>
+
+<p><strong>Note:</strong> You might have to reload the page to see the value of the cookie.</p>
+
+</body>
+</html>
+
+<!-- 
+Note: The setcookie() function must appear BEFORE the <html> tag.
+
+Note: The value of the cookie is automatically URLencoded when sending the cookie, and automatically decoded when received (to prevent URLencoding, use setrawcookie() instead).
+
+
  -->
