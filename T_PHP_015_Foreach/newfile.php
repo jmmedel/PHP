0455afc65e211068ea3a12b48ac89425f1220783
john <jm.kagaya@gmail.com>
2018-03-09 02:35:27 +0900

    Done

---
+++ T_PHP_015_Foreach/newfile.php
@@ -0,0 +1,20 @@
+
+
+
+
+<?php
+    
+    //Loop
+    //While loop
+    //Do while loop
+    // For loop
+    //Foreach loop
+  
+ $array = array("Kagaya","Jane","Medel", "Jake");
+ foreach ($array as $loopdata){
+     echo "My name is ".$loopdata."<br>";
+ }
+  
+
+
+?>
