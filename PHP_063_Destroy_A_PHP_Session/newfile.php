44ca0306330195df398ff08a127b86e50c6e17cc
john <jm.kagaya@gmail.com>
2018-03-08 16:54:58 +0900

    Done

---
+++ PHP_063_Destroy_A_PHP_Session/newfile.php
@@ -0,0 +1,31 @@
+
+
+<!-- 
+
+Destroy a PHP Session
+To remove all global session variables
+ and destroy the session, use session_unset() 
+ and session_destroy():
+ 
+ -->
+ 
+ 
+ <?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// remove all session variables
+session_unset(); 
+
+// destroy the session 
+session_destroy();
+
+echo "All session variables are now removed, and the session is destroyed." 
+?>
+
+</body>
+</html>
