6ca07bfc7a20b8b0abdc5de61db6ea8475d2124a
john <jm.kagaya@gmail.com>
2018-03-09 01:02:34 +0900

    Done

---
+++ T_PHP_004_Operators/newfile.php
@@ -0,0 +1,26 @@
+
+
+<html>
+<body>
+
+<?php 
+ // Arithmetic Operator;
+ echo 5%5;
+ echo "<br>";
+ echo 5+5;
+ echo "<br>";
+ echo 5**5;
+ echo "<br>";
+ echo 5+5;
+ echo "<br>";
+ echo 5-5;
+ echo "<br>";
+ // Assigment Operators
+  $x = 100;
+  $x += 20;
+ echo $x;
+ 
+?>
+
+</body>
+</html>
