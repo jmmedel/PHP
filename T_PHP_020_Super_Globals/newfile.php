6a90184a6f7690995645d019aaa08fea9f63b50e
john <jm.kagaya@gmail.com>
2018-03-09 03:47:31 +0900

    Done

---
+++ T_PHP_020_Super_Globals/newfile.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * $GLOBALS
+ * $QPOST
+ * $QGET
+ * $QCOOKIE
+ * $_SESSION
+ */
+
+$x = 5;
+function something()
+{
+    $y = 10;
+   echo $GLOBALS['x'];
+    
+    
+}
+
+something();
+?>
