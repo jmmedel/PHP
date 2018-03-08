6b6f4065b1d403fba6cdf141443896afeea8c06d
john <jm.kagaya@gmail.com>
2018-03-09 02:22:48 +0900

    Done

---
+++ T_PHP_012_While_Loop/newfile.php
@@ -0,0 +1,16 @@
+<?php
+    
+    //Loop
+    //While loop
+    //Do while loop
+    // For loop
+    //Foreach loop
+   $x = 1;
+   
+while($x < 5){
+   echo "hi there <br>";
+   $x++;
+   
+}
+
+?>
