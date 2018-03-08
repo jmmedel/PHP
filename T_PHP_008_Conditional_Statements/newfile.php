e7a22234123a8624d420824406975e92fb74c82f
john <jm.kagaya@gmail.com>
2018-03-09 01:26:21 +0900

    Done

---
+++ T_PHP_008_Conditional_Statements/newfile.php
@@ -0,0 +1,17 @@
+
+<!--  Conditional Statemets  -->
+<?php
+    
+
+    $x = 2;
+    if($x == 1){
+        echo "This is True";
+    }
+    elseif ($x == 2){
+        echo "Use elseif";
+    }
+    else {
+        echo "Else statements";
+    }
+
+?>
