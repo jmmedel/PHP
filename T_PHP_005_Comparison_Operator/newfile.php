dd48b4f0aa537abbf496d5b2b224237802f24b39
john <jm.kagaya@gmail.com>
2018-03-09 01:05:54 +0900

    Done

---
+++ T_PHP_005_Comparison_Operator/newfile.php
@@ -0,0 +1,19 @@
+
+
+
+
+<?php 
+    
+    //Comparison Operators
+    // > < = == 
+    $x = 5;
+    $y = 10;
+    if($x > $y){
+        echo "True";
+    }
+    else {
+        echo "False";
+    }
+
+
+?>
