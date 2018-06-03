06000dc7dc244c6fd7cbb153f72383c9177a4109
kjmkagaya <jm.kagaya@gmail.com>
2018-06-04 07:06:22 +0900

    Done
    Done

--- Project/03_PHP_Quizzer/06_Add_Question_Form/Program-Files/Program Files/quizzer/database.php
+++ Project/03_PHP_Quizzer/06_Add_Question_Form/Program-Files/Program Files/quizzer/database.php
@@ -1,9 +1,9 @@
 <?php
 //Create connection credentials
 $db_host = 'localhost';
-$db_name = 'quizzer';
+$db_name = 'shouts';
 $db_user = 'root';
-$db_pass = '121212aa';
+$db_pass = '';
 
 //Create mysqli object
 $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
