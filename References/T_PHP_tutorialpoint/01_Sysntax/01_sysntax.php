57580cd3594e7f5e68c815a33bfabf69c2344560
kjmkagaya <jm.kagaya@gmail.com>
2018-06-25 10:08:14 +0900

    Done
    Done

---
+++ References/T_PHP_tutorialpoint/01_Sysntax/01_sysntax.php
@@ -0,0 +1,26 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <!-- 
+
+        The PHP Hypertext Preprocessor (PHP) is a programming language that allows web developers to create dynamic content that interacts with databases. PHP is basically used for developing web based software applications. This tutorial helps you to build your base with PHP.
+
+Audience
+This tutorial is designed for PHP programmers who are completely unaware of PHP concepts but they have basic understanding on computer programming.
+
+Prerequisites
+Before proceeding with this tutorial you should have at least basic understanding of computer programming, Internet, Database, and MySQL etc is very helpful.
+
+    -->
+</body>
+</html>
