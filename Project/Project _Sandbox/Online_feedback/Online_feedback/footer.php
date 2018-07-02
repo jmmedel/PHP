2659775f688eb988f09f3694e5a452a03f34b85d
kjmkagaya <jm.kagaya@gmail.com>
2018-07-02 15:38:13 +0900

    Done
    Done

--- Project/Project _Sandbox/Online_feedback/Online_feedback/footer.php
+++ Project/Project _Sandbox/Online_feedback/Online_feedback/footer.php
@@ -2,8 +2,14 @@
 
 
 <?php
-
-echo"<div class=navbar-fixed-bottom nav navbar-inverse text-center style=padding:15px;height:40px; background:#66CCFF>		<span style=color:#FFFFFF> KJM	<a href=https://github.com/jmmedel>KJM</a> </span></div> "
-
+echo '
+<footer>
+<div class="container text-center">
+    Copyright &copy; 2014, PHP Kagaya
+</div>
+</footer>
+ '
+/*echo"<div class=navbar-fixed-bottom nav navbar-inverse text-center style=padding:15px;height:40px; background:#66CCFF>		<span style=color:#FFFFFF> KJM	<a href=https://github.com/jmmedel>KJM</a> </span></div> "
+*/
 
 ?>
