2ed94dcac917af20c47bfb12addc132ee9ed1015
kjmkagaya <jm.kagaya@gmail.com>
2018-07-03 11:38:51 +0900

    Done
    Done

--- Project/Project _Sandbox/madmin/users.php
+++ Project/Project _Sandbox/madmin/users.php
@@ -14,77 +14,8 @@
 </head>
 
 <body class="grey lighten-4">
-  <nav class="blue darken-2">
-    <div class="container">
-      <div class="nav-wrapper">
-        <a href="index.php" class="brand-logo">Madmin</a>
-        <a href="#" data-activates="side-nav" class="button-collapse show-on-large right">
-          <i class="material-icons">menu</i>
-        </a>
-        <ul class="right hide-on-med-and-down">
-          <li>
-            <a href="index.php">Dashboard</a>
-          </li>
-          <li>
-            <a href="posts.php">Posts</a>
-          </li>
-          <li>
-            <a href="categories.php">Categories</a>
-          </li>
-          <li>
-            <a href="comments.php">Comments</a>
-          </li>
-          <li class="active">
-            <a href="users.php">Users</a>
-          </li>
-        </ul>
-        <!-- Side nav -->
-        <ul id="side-nav" class="side-nav">
-          <li>
-            <div class="user-view">
-              <div class="background">
-                <img src="img/ocean.jpg" alt="">
-              </div>
-              <a href="#">
-                <img src="img/person1.jpg" alt="" class="circle">
-              </a>
-              <a href="#">
-                <span class="name white-text">John Doe</span>
-              </a>
-              <a href="#">
-                <span class="email white-text">jdoe@gmail.com</span>
-              </a>
-            </div>
-          </li>
-          <li>
-            <a href="index.php">
-              <i class="material-icons">dashboard</i> Dashboard</a>
-          </li>
-          <li>
-            <a href="posts.php">Posts</a>
-          </li>
-          <li>
-            <a href="categories.php">Categories</a>
-          </li>
-          <li>
-            <a href="comments.php">Comments</a>
-          </li>
-          <li>
-            <a href="users.php">Users</a>
-          </li>
-          <li>
-            <div class="divider"></div>
-          </li>
-          <li>
-            <a class="subheader">Account Controls</a>
-          </li>
-          <li>
-            <a href="login.php" class="waves-effect">Logout</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+   <!-- NavVar -->
+   <?php include 'nav.php';?>
 
   <!-- Section: Users -->
   <section class="section section-users grey lighten-4">
