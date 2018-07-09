31c5fce28ca8d0ddc5be0df5d349853bf7987c6b
kjmkagaya <jm.kagaya@gmail.com>
2018-07-09 14:29:04 +0900

    Done
    Done

--- Project/Project _Sandbox/Online_Shopping/Online_Shopping/config.php
+++ Project/Project _Sandbox/Online_Shopping/Online_Shopping/config.php
@@ -1,4 +1,5 @@
 <?php
-mysql_connect("localhost","root","");
-mysql_select_db("shop");
+//mysql_connect("localhost","root","");
+//mysql_select_db("shop");
+mysqli_connect("localhost","root","","shop")or die(mysqli_error());
 ?>
