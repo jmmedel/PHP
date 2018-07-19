20e89c5aba3147d5292c2b408d2fc6472486c255
kjmkagaya <jm.kagaya@gmail.com>
2018-07-19 12:58:43 +0900

    Done
    Done

---
+++ 01_Laravel/04_Laravel Middleware/app/Http/Controllers/ABCController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use app\Http\Requests;
+use app\Http\Controllers\Controller;
+class ABCController extends Controllers
+{
+    //
+    public function index(){
+        echo "<br>ABC Controller.";
+     }
+     
+}
