40ee73375f89ad1ca072e2e5790683be064f9904
john <jm.kagaya@gmail.com>
2018-07-25 14:44:42 +0900

    Done
    Done

--- PHP_XAMPP/PHP_SandBox2/phpsandbox/website8/inc/header.php
+++ PHP_XAMPP/PHP_SandBox2/phpsandbox/website8/inc/header.php
@@ -2,7 +2,7 @@
 	<html>
 		<head>
 			<title>PHP Blog</title>
-			<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.2/flatly/bootstrap.min.css">
+			<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.2/cerulean/bootstrap.min.css">
 		</head>
 	<body>
 	<?php include('navbar.php'); ?>
