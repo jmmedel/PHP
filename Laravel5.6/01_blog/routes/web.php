05f0d650b687c057cc235ef13edcb67ce8cee30c
john <jm.kagaya@gmail.com>
2018-07-31 22:58:18 +0900

    Done
    done

--- Laravel5.6/01_blog/routes/web.php
+++ Laravel5.6/01_blog/routes/web.php
@@ -12,8 +12,13 @@
 */
 
 Route::get('/','PublicController@index' );
-Route::get('/post{id}','PublicController@singlePost');
+// all ways / on the {id} 
+Route::get('/post/{id}','PublicController@singlePost');
 Route::get('/about','PublicController@about');
 Route::get('/contact','PublicController@contact');
 Route::post('/contact','PublicController@contactPost');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
