783ff703f7f266f9a282f42a61d49e988a57d80c
john <jm.kagaya@gmail.com>
2018-08-02 01:56:29 +0900

    Done
    done

--- Laravel5.6/01_blog/app/Http/Controllers/PublicController.php
+++ Laravel5.6/01_blog/app/Http/Controllers/PublicController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Post;
 class PublicController extends Controller
 {
     //
     public function index(){
-        return view('welcome');
+        $posts = Post::all();
+        return view('welcome',compact('posts'));
     }
 
     public function singlePost($id){
--- Laravel5.6/01_blog/resources/views/welcome.blade.php
+++ Laravel5.6/01_blog/resources/views/welcome.blade.php
@@ -21,21 +21,26 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
+          @foreach($posts as $post)
           <div class="post-preview">
-            <a href="post.html">
+            <a href="#">
               <h2 class="post-title">
-                Man must explore, and this is exploration at its greatest
+                {{ $post->title }}
               </h2>
-              <h3 class="post-subtitle">
-                Problems look mighty small from 150 miles up
-              </h3>
+            
             </a>
             <p class="post-meta">Posted by
-              <a href="#">Start Bootstrap</a>
-              on September 24, 2018</p>
+              <a href="#">{{ $post->user->name }}</a>
+              on {{ date_format($post->created_at,'F d, Y')}}</p>
+
+              <p>
+                <i class="fa fa-comment" aria-hidden="true"></i>
+              </p>
+
           </div>
           <hr>
        
+          @endforeach
           <!-- Pager -->
           <div class="clearfix">
             <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
