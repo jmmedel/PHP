e02ca315633db363c43b7ee2068785687fccb249
john <jm.kagaya@gmail.com>
2018-08-01 12:35:16 +0900

    Done
    Done

--- Laravel5.6/01_blog/resources/views/auth/login.blade.php
+++ Laravel5.6/01_blog/resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<!-- 
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -71,21 +71,10 @@
     </div>
 </div>
 
---->
 
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Carbon - Admin Template</title>
-    <link rel="stylesheet" href="./vendor/simple-line-icons/css/simple-line-icons.css">
-    <link rel="stylesheet" href="./vendor/font-awesome/css/fontawesome-all.min.css">
-    <link rel="stylesheet" href="./css/styles.css">
-</head>
-<body>
+
+<!-- 
 <div class="page-wrapper flex-row align-items-center">
     <div class="container">
         <div class="row justify-content-center">
@@ -94,7 +83,8 @@
                     <div class="card-header text-center text-uppercase h4 font-weight-light">
                         Login
                     </div>
-
+                <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+                    @csrf
                     <div class="card-body py-5">
                         <div class="form-group">
                             <label class="form-control-label">Email</label>
@@ -111,7 +101,7 @@
                             <label class="custom-control-label" for="login">Check this custom checkbox</label>
                         </div>
                     </div>
-
+                </form>
                     <div class="card-footer">
                         <div class="row">
                             <div class="col-6">
@@ -128,14 +118,6 @@
         </div>
     </div>
 </div>
-<script src="./vendor/jquery/jquery.min.js"></script>
-<script src="./vendor/popper.js/popper.min.js"></script>
-<script src="./vendor/bootstrap/js/bootstrap.min.js"></script>
-<script src="./vendor/chart.js/chart.min.js"></script>
-<script src="./js/carbon.js"></script>
-<script src="./js/demo.js"></script>
-</body>
-</html>
-
+--->
 
 @endsection
