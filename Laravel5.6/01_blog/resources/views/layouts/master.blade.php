06b87ceda628821053a75c5a020d1024fbf67571
john <jm.kagaya@gmail.com>
2018-08-04 14:31:59 +0900

    Done
    Done

--- Laravel5.6/01_blog/resources/views/layouts/master.blade.php
+++ Laravel5.6/01_blog/resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="blog ">
+    <meta name="description" content="blog posts ">
     <meta name="author" content="kagaya john ">
 
     <title>@yield('title','kagaya blog')</title>
