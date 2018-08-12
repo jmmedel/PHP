0a83185f1aebce03b64459163f1af06e01e0251f
john <jm.kagaya@gmail.com>
2018-08-12 15:26:03 +0900

    Done
    done

--- Laravel5.6/01_blog/resources/views/contact.blade.php
+++ Laravel5.6/01_blog/resources/views/contact.blade.php
@@ -8,7 +8,7 @@
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
             <div class="page-heading">
-              <h1>Contact Me</h1>
+              <h1>Contact Me In jm.yahoo@.com</h1>
               <span class="subheading">Have questions? I have answers.</span>
             </div>
           </div>
