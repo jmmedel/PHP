eba8b0e11cc7c34c6d849c36d4ff8cfd52968169
john <jm.kagaya@gmail.com>
2018-08-13 21:55:01 +0900

    Done
    done

--- Laravel5.6/01_blog/resources/views/includes/admin/sidebarNavigation.blade.php
+++ Laravel5.6/01_blog/resources/views/includes/admin/sidebarNavigation.blade.php
@@ -5,13 +5,13 @@
                 <li class="nav-title">Navigation</li>
 
                 <li class="nav-item">
-                    <a href="{{ route('userDashboard') }}" class="nav-link active">
+                    <a href="{{ route('userDashboard') }}" class="nav-link {{ Route::currentRouteName() == 'userDashboard' ? 'active' : ''}}">
                         <i class="icon icon-speedometer"></i> Dashboard
                     </a>
                 </li>
 
                 <li class="nav-item nav-dropdown">
-                    <a href="{{ route('userComments') }}" class="nav-link ">
+                    <a href="{{ route('userComments') }}" class="nav-link {{ Route::currentRouteName() == 'userComments' ? 'active' : ''}} ">
                         <i class="icon icon-book-open"></i> Comments
                     </a>
 
@@ -23,19 +23,19 @@
                 <li class="nav-title">Author</li>
 
                 <li class="nav-item nav-dropdown">
-                    <a href="{{ route('authorDashboard') }}" class="nav-link ">
+                    <a href="{{ route('authorDashboard') }}" class="nav-link {{ Route::currentRouteName() == 'authorDashboard' ? 'active' : ''}} ">
                         <i class="icon icon-speedometer"></i> Dashboard
                     </a>  
                 </li>
 
                 <li class="nav-item nav-dropdown">
-                        <a href="{{ route('authorPosts') }}" class="nav-link ">
+                        <a href="{{ route('authorPosts') }}" class="nav-link  {{ Route::currentRouteName() == 'authorPosts' ? 'active' : ''}} ">
                             <i class="icon icon-paper-clip"></i> Posts
                         </a>  
                 </li>
 
                 <li class="nav-item nav-dropdown">
-                        <a href="{{ route('authorComments') }}" class="nav-link ">
+                        <a href="{{ route('authorComments') }}" class="nav-link  {{ Route::currentRouteName() == 'authorComments' ? 'active' : ''}} ">
                             <i class="icon icon-book-open"></i> Comments 
                         </a>  
                 </li>
@@ -43,26 +43,26 @@
                 <li class="nav-title">Admin</li>
 
                 <li class="nav-item nav-dropdown">
-                        <a href="{{ route('adminDashboard') }}" class="nav-link ">
+                        <a href="{{ route('adminDashboard') }}" class="nav-link  {{ Route::currentRouteName() == 'adminDashboard' ? 'active' : ''}} ">
                             <i class="icon icon-speedometer"></i> Dashboard
                         </a>  
                 </li>
 
                 <li class="nav-item nav-dropdown">
-                        <a href="{{ route('adminPosts') }}" class="nav-link ">
+                        <a href="{{ route('adminPosts') }}" class="nav-link  {{ Route::currentRouteName() == 'adminPosts' ? 'active' : ''}} ">
                             <i class="icon icon-paper-clip"></i> Posts
                         </a>  
                 </li>
 
                 <li class="nav-item nav-dropdown">
-                        <a href="{{ route('adminComments') }}" class="nav-link ">
+                        <a href="{{ route('adminComments') }}" class="nav-link  {{ Route::currentRouteName() == 'adminComments' ? 'active' : ''}} ">
                             <i class="icon icon-book-open"></i> Comments 
                         </a>  
                 </li>
 
 
                 <li class="nav-item nav-dropdown">
-                        <a href="{{ route('adminUsers') }}" class="nav-link ">
+                        <a href="{{ route('adminUsers') }}" class="nav-link {{ Route::currentRouteName() == 'adminUsers' ? 'active' : ''}}  ">
                             <i class="icon icon icon-user"></i> Users
                         </a>  
                 </li>
