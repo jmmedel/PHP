e0cdf5595a54d9f0f4a3b846594bb16cce3e3efd
john <jm.kagaya@gmail.com>
2018-08-16 04:44:36 +0900

    Done
    Done

--- Laravel5.6/01_blog/resources/views/user/comments.blade.php
+++ Laravel5.6/01_blog/resources/views/user/comments.blade.php
@@ -30,7 +30,9 @@
                                 <td class="text-nowrap"><a href="{{ route('singlePost',$comment->id) }}">{{ $comment->post->title }}</a></td>
                                 <td>{{ $comment->content }}</td>
                                 <td>{{ \Carbon\Carbon::parse($comment->created_at)->diffForHumans() }}</td>
-                                <td><a href="#">X</a></td>
+                                <td>
+                                    <a href="#">X</a>
+                                </td>
                             </tr>
                            
                             @endforeach
